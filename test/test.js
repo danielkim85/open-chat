b2c05b7f14067d403667a9170114d6e550578263
DK <danielkim@soulkast.com>
2018-12-27 01:23:57 -0500

    added socket io and its client. added ping test.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,29 @@
 const sooda = require('../lib/sooda');
 
-console.info(sooda);
-console.info(sooda.ping());
+//kick off express server
+const app = require('express')();
+const http = require('http').Server(app);
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+
+  //kick off sooda
+  sooda.init(http);
+
+  //client connection test
+  const io = require('socket.io-client');
+  const socket = io.connect('http://localhost:3000');
+
+  //connect and begin testing
+  socket.once('connected', function(){
+    //ping test
+    sooda.ping(socket);
+
+    //terminate after pong!
+    socket.on('pong!',function(msg){
+      console.info(msg);
+      console.warn('terminating ...');
+      process.exit(0);
+    })
+  });
+});
